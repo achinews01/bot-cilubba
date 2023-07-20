4febb74026991bb7315962fb66150e759de12676
achinews01 <128749733+achinews01@users.noreply.github.com>
2023-07-20 19:49:28 +0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const {
+   indonesia
+} = require("./language");
+
+// Website Api (jgn di ganti biar gk eror)
+global.APIs = {
+   alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
+}
+
+//buy apikey premium 0887435047326
+// Free apikey (silahkan login terus ganti Your Key dgn apikey lu)
+global.APIKeys = {
+   'https://api.zeeoneofc.my.id': 'Your Key', // 👉 login DicOX9DvI4fmYaM to get apikey
+}
+
+//language 
+global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+
+global.BOT_TOKEN = "5984051534:AAGGc_H1hsJDEFDjjfwb2mKa3k9Gq4XkLmo" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "Alphabot-Mdོ" //your bot name
+global.OWNER_NAME = "ᴹᴿ᭄ CilubbaTVོ ×፝֟͜×" //your name
+global.OWNER_NUMBER = "6281523645640" //your telegram number
+global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
+global.lang = language //don't change
